ab784c4164dd9ab8a2cef50d932654f734a5e4cb
Ahmed Adel <kingahmedadel@hotmail.com>
2023-07-17 09:42:24 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+	char alph[26]="abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for( i=0;i<26<i++){
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return(0);
+}
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+        char alph[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+        for( i=0;i<52<i++){
+                putchar(alph[i]);
+        }
+        putchar('\n');
+        return(0);
+}
