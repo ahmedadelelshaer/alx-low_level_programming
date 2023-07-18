934eb587a73bd950fcb14a0a2ee12042b98c08c0
Ahmed Adel <kingahmedadel@hotmail.com>
2023-07-18 06:58:35 -0700

    aa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include <main.h>
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+nclude "main.h"
+
+/**
+ *  * times_table - prints the 9 times table, starting with 0
+ *   */
+void times_table(void)
+{
+}
